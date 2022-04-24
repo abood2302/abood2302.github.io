74e209bd5223dda2ba42b89019e108041d21cfa1
Yosuf Mohamed Hassan <yosuf_se@tutanota.com>
2022-04-24 18:40:13 +0800

    fix pwa

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,18 +1,26 @@
+cacheArray = [
+    './assets/android-chrome-192x192.png',
+    './assets/apple-touch-icon.png',
+    './assets/favicon-16x16.png',
+    './assets/favicon-32x32.png',
+    './assets/favicon.png',
+    './assets/logo.webp',
+    './index.html',
+    './index.js',
+    './style.css'
+]
+
 self.addEventListener('install', e => {
     console.log('Service worker installed')
     e.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAll(["./", "./assets/favicon.png","./assets/logo.webp","./style.css","./index.js","./assets/android-chrome-192x192.png"])
+            return cache.addAll(cacheArray)
         })
     )
 });
 
 self.addEventListener('fetch', e => {
-    console.log(`Intercepting Fetch request for ${e.request.url}`);
-    e.respondWith(
-        caches.match(e.request).then(response => {
-            return response || fetch(e.request)
-        })
-    )
-})
+  console.log('Service Worker: Fetching');
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
 
