16ade220c0c994f45ff87b762646b7f9b2ae84b4
abood2302 <104266933+abood2302@users.noreply.github.com>
2022-06-01 12:54:15 +0800

    fini

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('serviceWorker.js').then(registeration=>{
+    navigator.serviceWorker.register('serviceWorker.js').then(registeration => {
         console.log("Service worker registered")
-    }).catch(error=>{
+    }).catch(error => {
         console.log("Service worker registeration failed");
         console.log(error);
     })
@@ -11,31 +11,30 @@ const sh_sm = document.getElementById('small')
 const sh_sm_count = document.getElementById('shawerma-small-count');
 const sh_sm_add = document.getElementById('sh-sm-add')
 const sh_sm_remove = document.getElementById("sh-sm-remove")
-
 const sh_sm_ch = document.getElementById('small-cheese')
-
-
+const sh_sm_ch_remove = document.getElementById('sh-sm-ch-remove')
 const sh_l = document.getElementById('large')
-
-
+const sh_la_remove = document.getElementById('sh-la-remove')
 const sh_l_ch = document.getElementById('large-cheese')
-
-
+const sh_la_ch_remove = document.getElementById('sh-la-ch-remove')
 const sh_ar = document.getElementById("arabic")
-
-
+const sh_ar_remove = document.getElementById('sh-ar-remove')
 const sh_ar_ch = document.getElementById('arabic-cheese')
-
-
+const ar_ch_remove = document.getElementById('ar-ch-remove')
 const frise = document.getElementById('fries')
-
-
+const fries_remove = document.getElementById('fries-remove')
 const sh_total = document.querySelector('#shwarma-total')
 const piiza_butoon = document.querySelector('#pizza')
+const pizza_remove = document.getElementById('pizza_remove')
 const pizza_total = document.querySelector('#pizza_total')
+const chiken = document.getElementById('chiken')
+const chic_remove = document.getElementById('chic_remove')
 const pa_five = document.querySelector('#pa_5')
+const pastries5_remove = document.getElementById('pastries5_remove')
 const pa_three = document.querySelector('#pa_3')
+const pastries3_remove = document.getElementById('pastries3_remove')
 const pa_tow = document.querySelector('#pa_2')
+const pastries2_remove = document.getElementById('pastries2_remove')
 const square_total = document.querySelector('#square')
 const dayTotal = document.getElementById('daytotal')
 const show = document.getElementById('show-info')
@@ -43,21 +42,9 @@ const rest = document.getElementById("rest")
 const byMonth = document.getElementById('getbymonth')
 const month = document.getElementById('month')
 let input = document.querySelector('input')
-const submit = document.querySelector('.submit')
-
-// get current month and update button text dynamically
-const currentMonth = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
-
-const d = new Date();
-let monthtext = currentMonth[d.getMonth()];
-
-byMonth.textContent = monthtext + " sales report"
-
-currentdate = new Date()
-datestringelement = document.getElementById('datestring')
-dateString = `${new Date().toLocaleDateString('en-GB', { weekday: 'short' })} ${currentdate.getDate()} ${monthtext} ${currentdate.getFullYear()}`
-datestringelement.textContent = dateString
-
+const submit = document.getElementById('submit')
+const totalorder = document.getElementById('totalorder');
+const showOrderInfo = document.getElementById('show_totalorder')
 
 
 const lastTotalAtTheDay = []
@@ -65,74 +52,126 @@ const lastTotalAtTheDay = []
 // shwarma button logic
 
 let totalShawrmaByDay = 0;
-var sh_sm_temp = 0
+let sh_temp = 0
+let pizza_temp = 0;
+let totalOrder = 0;
 
 sh_sm.addEventListener('click', () => {
     totalShawrmaByDay += 6
-    sh_sm_temp = sh_sm_temp + 1
-    if (sh_sm_temp>0) {
-        sh_sm_count.textContent = sh_sm_temp
-    } else {
-        sh_sm_count.textContent = '0'
-    }
+    totalOrder += 6
 })
 
-sh_sm_add.addEventListener('click', () => {
-    totalShawrmaByDay += 6
-    sh_sm_temp = sh_sm_temp + 1
-    if (sh_sm_temp>0) {
-        sh_sm_count.textContent = sh_sm_temp
-    } else {
-        sh_sm_count.textContent = '0'
-    }
-})
 
 sh_sm_remove.addEventListener('click', () => {
     totalShawrmaByDay -= 6
-    sh_sm_temp = sh_sm_temp -1
-    if (sh_sm_temp>0) {
-        sh_sm_count.textContent = sh_sm_temp
-    } else {
-        sh_sm_count.textContent = '0'
-    }
+
+    totalOrder -= 6
 })
 
 sh_sm_ch.addEventListener('click', () => {
     totalShawrmaByDay += 7
+    totalOrder += 7
     console.log(totalShawrmaByDay);
+})
+
+sh_sm_ch_remove.addEventListener('click', () => {
+    totalShawrmaByDay -= 7
+
+    totalOrder -= 7
+
 })
 
 sh_l.addEventListener('click', () => {
     totalShawrmaByDay += 9
+
     console.log(totalShawrmaByDay);
+    totalOrder += 9
+})
+
+sh_la_remove.addEventListener('click', () => {
+    totalShawrmaByDay -= 9
+
+
+    totalOrder -= 9
 })
 
 sh_l_ch.addEventListener('click', () => {
     totalShawrmaByDay += 11
+
     console.log(totalShawrmaByDay);
+    totalOrder += 11
+})
+
+sh_la_ch_remove.addEventListener('click', () => {
+    totalShawrmaByDay -= 11
+
+    totalOrder -= 11
 })
 
 sh_ar.addEventListener('click', () => {
     totalShawrmaByDay += 16
+
     console.log(totalShawrmaByDay);
+    totalOrder += 16
+})
+
+sh_ar_remove.addEventListener('click', () => {
+    totalShawrmaByDay -= 16
+
+
+    totalOrder -= 16
 })
 
 sh_ar_ch.addEventListener('click', () => {
     totalShawrmaByDay += 18
+
     console.log(totalShawrmaByDay);
+    totalOrder += 18
+})
+ar_ch_remove.addEventListener('click', () => {
+    totalShawrmaByDay -= 18
+    totalOrder -= 18
 })
 
 frise.addEventListener('click', () => {
     totalShawrmaByDay += 6
     console.log(totalShawrmaByDay);
+    totalOrder += 6
+})
+
+fries_remove.addEventListener('click', () => {
+    totalShawrmaByDay -= 6
+
+    totalOrder -= 6
 })
 
 // pizza button
 let totalPizza = 0;
 piiza_butoon.addEventListener('click', () => {
-    totalPizza += 16 - 1 // pizza button decrement to accomadate for the starting value of 1
+    totalPizza += 16
+    totalOrder += 16
     console.log(totalPizza);
 });
+
+pizza_remove.addEventListener('click', () => {
+    totalPizza -= 16
+    totalOrder -= 16
+    console.log(totalPizza);
+})
+
+chiken.addEventListener('click', () => {
+    totalPizza += 17;
+    totalOrder += 17
+    console.log(totalPizza);
+})
+
+chic_remove.addEventListener('click', () => {
+    totalPizza -= 17
+    totalOrder -= 17
+    console.log(totalPizza);
+})
+
+
 
 
 // input.
@@ -142,7 +181,6 @@ let submitTotal = 0;
 submit.addEventListener('click', () => {
     let inputValue = +input.value
     submitTotal += inputValue
-    // console.log(submitTotal.typeof);
     console.log(inputValue);
     console.log(submitTotal);
 })
@@ -153,20 +191,49 @@ let totalPastries = 0;
 
 pa_five.addEventListener('click', () => {
     totalPastries += 5
+    totalOrder += 5
+    console.log(totalPastries);
+})
+
+pastries5_remove.addEventListener('click', () => {
+    totalPastries -= 5
+    totalOrder -= 5
     console.log(totalPastries);
 })
 
 pa_three.addEventListener('click', () => {
     totalPastries += 3
+    totalOrder += 3
     console.log(totalPastries);
 })
+
+pastries3_remove.addEventListener('click', () => {
+    totalPastries -= 3
+    totalOrder -= 3
+    console.log(totalPastries);
+})
+
 
 
 pa_tow.addEventListener('click', () => {
     totalPastries += 2
+    totalOrder += 2
     console.log(totalPastries);
 })
 
+pastries2_remove.addEventListener('click', () => {
+    totalPastries -= 2
+    totalOrder -= 2
+    console.log(totalPastries);
+})
+
+
+totalorder.addEventListener('click', () => {
+    showOrderInfo.innerHTML = totalOrder
+    totalOrder = 0
+})
+
+showOrderInfo.innerHTML = ' total order'
 
 let totalDaySell = 0;
 dayTotal.addEventListener('click', () => {
@@ -186,11 +253,7 @@ rest.addEventListener('click', () => {
     totalPizza = 0
     totalShawrmaByDay = 0
     submitTotal = 0
-    // reset counter text
-    sh_sm_count.textContent ='';
-    // reset temp variables
-    sh_sm_temp = 0
-    // fix xss bug
+
     show.textContent = 'Daily Sales'
 
 })
@@ -200,8 +263,6 @@ let monthSell = 0;
 // console.log(newArry);
 // console.log(lastTotalAtTheDay);
 byMonth.addEventListener('click', () => {
-
-
     const arr = (JSON.parse(localStorage.getItem('item1')))
     for (let i = 0; i < arr.length; i++) {
         let x = arr[i];
@@ -209,6 +270,7 @@ byMonth.addEventListener('click', () => {
     }
     month.innerHTML = monthSell
     console.log(arr);
+    localStorage.removeItem('item1')
 
 
 });
